a14bfddeadc87a100c1888a32595f7bb1a9d8b98
UserTCQ <rayngu2005@gmail.com>
2021-06-28 17:21:27 +0700

    Added all repos page

--- js/auto-site.js
+++ js/auto-site.js
@@ -4,26 +4,27 @@ let pages = [
     ["UnImage Object v2", "/unimageobjectv2.html"],
     ["PA Animator", "/paanimator.html"],
     ["PA Prefab Toolkit", "/paprefabtoolkit.html"],
+    ["All repositories", "/allrepos.html"],
     ["Enchariud", "/enchariud.html"],
     ["DataM", "/datam.html"]
 ];
 
-function init() {
-    let sidebar = document.getElementById("sidebar");
-    for (let i = 0; i < pages.length; i++) {
-        let page = pages[i];
-        sidebar.innerHTML += "<div class=\"sidebar-button\" onclick=\"window.location.href = '" + page[1] + "';\"><div class=\"red-line-sidebar-btn\" ></div><div class=\"sidebar-text\"><span>" + page[0] + "</span></div></div>";
-    }
-
-    let currentPagePath = window.location.pathname;
-    let currentPageIndex = 0;
-    for (let i = 0; i < pages.length; i++) {
-        if (pages[i][1] == currentPagePath) {
-            currentPageIndex = i;
-            break;
-        }
-    }
-
-    document.title = pages[currentPageIndex][0] + " | Reimnop";
-    document.getElementById("header-text").innerHTML = pages[currentPageIndex][0];
+let sidebar = document.getElementById("sidebar");
+for (let i = 0; i < pages.length; i++) {
+    let page = pages[i];
+    sidebar.innerHTML += 
+        `<a class="sidebar-button" href="` + page[1] + `">
+            <div class="red-line-sidebar-btn" ></div>` + 
+            page[0] + 
+        `</a>`;
 }
+let currentPagePath = window.location.pathname;
+let currentPageIndex = 0;
+for (let i = 0; i < pages.length; i++) {
+    if (pages[i][1] == currentPagePath) {
+        currentPageIndex = i;
+        break;
+    }
+}
+document.title = pages[currentPageIndex][0] + " | Reimnop";
+document.getElementById("header-text").innerHTML = pages[currentPageIndex][0];
---
+++ js/get-repos.js
@@ -0,0 +1,28 @@
+//https://api.github.com/users/Reimnop/repos
+$.getJSON("/data.json", function(repos, status) {
+    let content = document.getElementsByClassName("content")[0];
+    for (let i = 0; i < repos.length; i++) {
+        let msec = Date.parse(repos[i].pushed_at);
+        let date = new Date(msec);
+
+        content.innerHTML += 
+            `<a href="` + repos[i].html_url + `" class="repo-button">
+                <div class="repo-red-line"></div>
+                <div class="repo-text-container">
+                    <b class="repo-name">` + repos[i].name + `</b>
+                    <i class="repo-desc">` + (repos[i].description == null ? "No description" : repos[i].description) + `</i>
+                    <i class="repo-last-commit">Last commit: ` + date.ddmmyyyy() + `
+                </div>
+            </a>`
+    }
+});
+
+Date.prototype.ddmmyyyy = function() {
+    let d = this.getDate();
+    let m = this.getMonth() + 1;
+
+    let day = (d > 9 ? "" : "0") + d;
+    let month = (m > 9 ? "" : "0") + m;
+
+    return day + "/" + month + "/" + this.getFullYear();
+}
