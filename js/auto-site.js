760f1d0a98335743ffa7c535d47cbd38138e5454
UserTCQ <rayngu2005@gmail.com>
2021-06-30 16:22:08 +0700

    Revert "adaptive layout"
    This reverts commit f07124466832cf3e4dfcd4eb83142f9150c121ff.

--- js/auto-site.js
+++ js/auto-site.js
@@ -1,12 +1,12 @@
 let pages = [
-    ["About me!", "index.html"],
-    ["LeaderEngine", "leaderengine.html"],
-    ["UnImage Object v2", "unimageobjectv2.html"],
-    ["PA Animator", "paanimator.html"],
-    ["PA Prefab Toolkit", "paprefabtoolkit.html"],
-    ["All repositories", "allrepos.html"],
-    ["Enchariud", "enchariud.html"],
-    ["DataM", "datam.html"]
+    ["About me!", "/index.html"],
+    ["LeaderEngine", "/leaderengine.html"],
+    ["UnImage Object v2", "/unimageobjectv2.html"],
+    ["PA Animator", "/paanimator.html"],
+    ["PA Prefab Toolkit", "/paprefabtoolkit.html"],
+    ["All repositories", "/allrepos.html"],
+    ["Enchariud", "/enchariud.html"],
+    ["DataM", "/datam.html"]
 ];
 
 let sidebar = document.getElementById("sidebar");
@@ -21,7 +21,7 @@ for (let i = 0; i < pages.length; i++) {
 let currentPagePath = window.location.pathname;
 let currentPageIndex = 0;
 for (let i = 0; i < pages.length; i++) {
-    if (currentPagePath.endsWith(pages[i][1])) {
+    if (pages[i][1] == currentPagePath) {
         currentPageIndex = i;
         break;
     }
