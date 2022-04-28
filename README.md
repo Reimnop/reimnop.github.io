1dbd50cedcb55d9df367d70779b4860ced2d4968
Reimnop <56994757+Reimnop@users.noreply.github.com>
2022-04-29 00:20:40 +0700

    Removed everything

--- README.md
+++
@@ -1,4 +0,0 @@
-## Reimnop's homepage
-This is the repo for my website
-
-Go to https://reimnop.github.io for the beauty
--- vault/rev1/assets/js/set-delay.js
+++
@@ -1,4 +0,0 @@
-let content_boxes = document.getElementsByClassName("collapse");
-for (let i = 0; i < content_boxes.length; i++) {
-    content_boxes[i].style.animationDelay = i * 0.4 + "s";
-}
