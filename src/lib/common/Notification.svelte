f7fef5cdab77455bdbe825e51c1cb150b8187fc2
Reimnop <rayngu2005@gmail.com>
2023-01-31 01:34:18 +0700

    Why must I always screw up

--- src/lib/common/Notification.svelte
+++ src/lib/common/Notification.svelte
@@ -4,7 +4,7 @@
     export let text: string;
 </script>
 
-<div class="absolute h-fit w-fit flex items-center gap-4 rounded-xl p-4 bg-neutral-700 shadow-xl z-10 bottom-8 left-1/2 -translate-x-1/2 overflow-hidden"
+<div class="fixed h-fit w-fit flex items-center gap-4 rounded-xl p-4 bg-neutral-700 shadow-xl z-10 bottom-8 left-1/2 -translate-x-1/2 overflow-hidden"
      transition:fly={{y:16, duration: 300}}>
     <slot />
     <h5>{text}</h5>
