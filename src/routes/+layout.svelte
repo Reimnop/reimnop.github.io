e4c36f7a6aef744e1b5b850ec27cb5f3988c773c
Reimnop <56994757+Reimnop@users.noreply.github.com>
2023-01-31 10:19:30 +0700

    Implement background workaround for mobile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,6 +16,8 @@
     <meta property="og:url" content="https://reimnop.com/" />
 </svelte:head>
 
+<!-- background element -->
+<div class="fixed h-screen w-screen bg-gradient-to-t from-neutral-900 to-black bg-fixed bg-no-repeat bg-cover -z-10" />
 <div class="min-h-screen">
     <NavBar bind:width={navWidth} />
     <div class="w-full h-full flex">
