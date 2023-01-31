75957dd63f1599845e1480820b2c50196c78afa1
Reimnop <56994757+Reimnop@users.noreply.github.com>
2023-01-31 10:21:20 +0700

    Updates

--- _app/immutable/chunks/3-1b61695f.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/aboutme/_page.svelte-7efe8975.js";export{t as component};
---
+++ _app/immutable/chunks/3-6744c18f.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/aboutme/_page.svelte-5284c619.js";export{t as component};
