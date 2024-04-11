cf594fa316d04e3bd60bddbb186b97e240200b01
Reimnop <rayngu2005@gmail.com>
2024-04-11 23:28:01 +0700

    Starting yet another fucking rewrite.

--- src/lib/common/Box.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    let clazz = "";
-    export { clazz as class };
-</script>
-
-<div class="bg-neutral-800 rounded-xl p-8 {clazz}">
-    <slot/>
-</div>
--- src/routes/games/McProfile.ts
+++
@@ -1,9 +0,0 @@
-export class McProfile {
-    name: string;
-    uuid: string;
-
-    constructor(name: string, uuid: string) {
-        this.name = name;
-        this.uuid = uuid;
-    }
-}
