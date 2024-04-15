10a06e09927569041e0789b56417636c0c13b56b
Reimnop <rayngu2005@gmail.com>
2024-04-15 19:30:45 +0700

    Change some icons and texts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
     import IntroBackground from "$lib/images/IntroBackground.svg";
     import Banner from "$lib/images/Banner.svg";
-    import Grid2x2 from "~icons/lucide/grid-2x2";
-    import NavCard from "./NavCard.svelte";
+    import Search from "~icons/lucide/search";
+    import Wrench from "~icons/lucide/wrench";
+    import Gamepad2 from "~icons/lucide/gamepad-2";
+    import NavCard from "$lib/components/NavCard.svelte";
 </script>
 
 <div class="relative w-screen h-screen grid place-items-center bg-repeat px-8" style="background-image: url('{IntroBackground}')">
@@ -16,15 +18,13 @@
             is a collection of Reimnop's personal projects.
         </h1>
         <div class="flex flex-col gap-8">
-            <h2><Grid2x2 class="inline-block align-text-bottom"/> Explore</h2>
+            <h2><Search class="inline-block align-text-bottom"/> Explore</h2>
             <nav class="grid md:grid-cols-2 gap-4">
-                <NavCard href="/pa" title="Project Arrhythmia" description="game 4 nerds!">
+                <NavCard href="/pa" title="Project Arrhythmia" description="Tools and mods for Project Arrhythmia.">
+                    <Wrench class="text-[5rem]"/>
                 </NavCard>
-                <NavCard href="/pa" title="Project Arrhythmia" description="game 4 nerds!">
-                </NavCard>
-                <NavCard href="/pa" title="Project Arrhythmia" description="game 4 nerds!">
-                </NavCard>
-                <NavCard href="/pa" title="Project Arrhythmia" description="game 4 nerds!">
+                <NavCard href="/games" title="Games" description="Fun browser games.">
+                    <Gamepad2 class="text-[5rem]"/>
                 </NavCard>
             </nav>
         </div>
